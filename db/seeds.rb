efb61e4631d94ceeeb727d4e223af584ecb4254e
Bart Dangus <brskythomas@gmail.com>
2014-08-29 21:39:48 +0000

    assoc. users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,15 @@
 require 'faker'
+
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
  
  # Create Posts
  50.times do
@@ -16,6 +27,11 @@ require 'faker'
      body: Faker::Lorem.paragraph
    )
  end
+
+ User.first.update_attributes!(
+  email: 'brskythomas@gmail.com',
+  password: 'noKEYS480',
+  )
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
