628019771a728c909f164d8b3b83e2f68d466d9d
Bart Dangus <brskythomas@gmail.com>
2014-09-04 19:43:01 +0000

    configured app for production emails2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Bloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #http://dangus_bloccit.heroku.com
+  config.action_mailer.default_url_options = { host: 'dangus-bloccit.herokuapp.com'}
 end
