c60a33eea6a233576e6ab6e4c281a0bd7773e784
Bart Dangus <brskythomas@gmail.com>
2014-09-05 05:01:19 +0000

    markdown stuff

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,15 @@ default_scope { order('created_at DESC')}
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+  def markdown_title
+    render_as_markdown title
+  end
+
+  def markdown_body
+    render_as_markdown body
+  end
+
+
+
 end
+
