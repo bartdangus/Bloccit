e42cea6e9ff205bb7557aa4d7d695a7b6921305b
Bart Dangus <brskythomas@gmail.com>
2014-10-20 21:48:49 +0000

    destroying with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
+
 
   def create
     
@@ -27,10 +29,14 @@ class CommentsController < ApplicationController
       authorize @comment
       if @comment.destroy
         flash[:notice] = "You successfully destroyed this shit."
-        redirect_to [@topic, @post]
+        
       else
         flash[:error] = "Nice try but you blew it, try again."
-        redirect_to [@topic, @post]
+        
+    end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
     end
   end
 end
